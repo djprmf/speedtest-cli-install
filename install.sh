a02b610d0d54d2b951c94c2a8eb5695e619de01e
Pedro Fernandes <prmf06@gmail.com>
2017-05-28 21:39:56 +0100

    First version

---
+++ install.sh
@@ -0,0 +1,4 @@
+wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+chmod +x speedtest-cli
+mv speedtest-cli /usr/local/bin/speedtest-cli
+chown root:root /usr/local/bin/speedtest-cli
